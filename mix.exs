b5c969f238ccb17e67257c4cfc9cebe3de387b11
Kip Cole <kipcole9@gmail.com>
2026-05-08 02:41:14 +1000

    Update version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Localize.MixProject do
   use Mix.Project
 
-  @version "0.26.0"
+  @version "0.27.0"
   @cldr_version_path "priv/localize/version"
   @localize_patch_version_path "priv/localize/localize_patch_version"
 
