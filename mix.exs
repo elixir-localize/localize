e102373cef49b28ac76875e12e5d30edc27a7659
Kip Cole <kipcole9@gmail.com>
2026-06-30 17:30:04 +1000

    Fix Language.display_name string/tag consistency and region fallback. Closes #36

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Localize.MixProject do
   use Mix.Project
 
-  @version "0.41.0"
+  @version "0.41.1"
   @cldr_version_path "priv/localize/version"
   @localize_patch_version_path "priv/localize/localize_patch_version"
 
