a1b0bafbcb31cd0180982cc212391c00cf46d7a7
Kip Cole <kipcole9@gmail.com>
2026-07-24 04:00:45 +0930

    Bump version and update changelog

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Localize.MixProject do
   use Mix.Project
 
-  @version "1.0.0-rc.3"
+  @version "1.0.0-rc.4"
   @cldr_version_path "priv/localize/version"
   @localize_patch_version_path "priv/localize/localize_patch_version"
 
